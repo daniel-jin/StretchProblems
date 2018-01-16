a5f3a3bdf663d4a1541dc3f32e610189fc4ef81e
daniel-jin <daniel.h.jin@gmail.com>
2018-01-15 18:45:23 -0600

    1/15/2018 - Handling Optionals practice

--- MyPlayground.playground/Contents.swift
+++ MyPlayground.playground/Contents.swift
@@ -3,3 +3,18 @@
 import UIKit
 
 
+// January 15, 2018 -- Handling Optionals
+class Person {
+    var name: String
+    var favoriteColor: String?
+    var favoriteMovie: String?
+    
+    init(name: String) {
+        self.name = name
+    }
+}
+
+let me = Person(name: "Dan")
+me.favoriteColor = "black"
+print(me.favoriteColor!)
+print(me.favoriteMovie)
